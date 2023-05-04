7c1726faa645c3abeaf31a4145965dd02ac2ad82
AlejandroPlanelles <alejandro.planelles@gmail.com>
2023-05-04 16:53:46 +0200

    primero

---
+++ proyecto.js
@@ -0,0 +1,31 @@
+function sum(op1,op2){
+    resultado = op1+op2;
+    console.log(resultado);
+    return resultado;
+}
+
+
+function subs(op1,op2){
+    resultado = op1-op2;
+    console.log(resultado);
+    return resultado;
+}
+
+
+function mult(op1,op2){
+    resultado = op1*op2;
+    console.log(resultado);
+    return resultado;
+}
+
+
+function div(op1,op2){
+    resultado = op1/op2;
+    console.log(resultado);
+    return resultado;
+}
+function cuad(n){
+    solucion = n*n;
+    console.log(solucion);
+    return solucion;
+}
